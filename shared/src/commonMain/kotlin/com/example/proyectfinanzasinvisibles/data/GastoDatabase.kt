7aad408b6f6c46f6057dd5ea59caf401fa24fb5c
missaelvg <missagarcia12@gmail.com>
2026-06-23 22:10:39 -0600

    Arreglo

--- shared/src/commonMain/kotlin/com/example/proyectfinanzasinvisibles/data/GastoDatabase.kt
+++ shared/src/commonMain/kotlin/com/example/proyectfinanzasinvisibles/data/GastoDatabase.kt
@@ -1,7 +1,5 @@
 package com.example.proyectfinanzasinvisibles.data
 
-// Usamos un objeto (Singleton) para que tanto la UI como el Servicio de Android
-// compartan la misma instancia de datos en memoria durante la ejecución.
 object GastoDatabase {
     private val gastos = mutableListOf<Gasto>(
         Gasto(1, "Oxxo - Antojos", 45.0, "Antojos"),
