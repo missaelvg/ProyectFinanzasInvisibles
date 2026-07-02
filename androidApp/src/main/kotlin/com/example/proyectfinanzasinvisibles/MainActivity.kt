17d6751ad1cb6606e6dfd10e9c91987b521b95af
2330848-jpg <2330848@upt.edu.mx>
2026-07-02 08:37:16 -0600

    Implementa tarea en segundo plano con WorkManager para sincronizar gastos con Firestore

--- androidApp/src/main/kotlin/com/example/proyectfinanzasinvisibles/MainActivity.kt
+++ androidApp/src/main/kotlin/com/example/proyectfinanzasinvisibles/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.proyectfinanzasinvisibles.sync.backend.WorkManagerScheduler
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        WorkManagerScheduler.programarSincronizacionPeriodica(applicationContext)
 
         setContent {
             App()
